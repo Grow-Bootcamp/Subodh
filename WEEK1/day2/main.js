5b87a2d843f26ede7266fbdbd1a69b352f3b4e68
subodh <shahsubodh166@gmail.com>
2026-09-02 20:55:57 +0545

    add promiseAsyncAwaitDemo and asyncTaskQueue function

--- WEEK1/day2/main.js
+++ WEEK1/day2/main.js
@@ -5,7 +5,7 @@ const hoisting = '== Hoisting =='
 const coreMethods = '== Core Data types Methods =='
 const timers = '== Timers =='
 const promiseAsyncAwait = '== Asynchronous code in JS =='
-const asyncTaskEngineCli = '== Async Task Engine Cli =='
+const asyncTaskQueueTitle = '== Async Task Queue =='
 
 
 const eventLoopDemo = function () {
@@ -182,21 +182,65 @@ const timersDemo = function () {
 
 const promiseAsyncAwaitDemo = function () {
     console.log(promiseAsyncAwait)
+    const fetchData = function (delay, label) {
+        return new Promise((resolve) => {
+            setTimeout(() => { resolve(`${label} resolves after ${delay}ms`) }, delay)
+        })
+    }
 
+    fetchData(500, 'Task 1')
+        .then((res) => {
+            console.log(res)
+            return fetchData(800, 'Task 2')
+        })
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((error) => console.error(`Error: ${error}`))
+        .finally(() => {
+            console.log('Promise Chain complete')
+        })
+
+    const runAsync = async function () {
+        let result1 = await fetchData(1000, 'Async Task 1')
+        console.log(result1)
+        let result2 = await fetchData(1200, 'Async Task 2')
+        console.log(result2)
+    }
+    runAsync();
 
     console.log(promiseAsyncAwait)
 }()
 
-const asyncTaskEngineCliDemo = function () {
-    console.log(asyncTaskEngineCli)
+const asyncTaskQueue = function () {
+    console.log(asyncTaskQueueTitle)
+    const tasks = [
+        { name: 'Download file', duration: 2000 },
+        { name: 'Process data', duration: 1500 },
+        { name: 'Save to DB', duration: 1000 },
+        { name: 'Send notification', duration: 800 },
+    ]
 
-    console.log(asyncTaskEngineCli)
+    const executeTask = (task) {
+        return new Promise((resolve) => {
+            console.log(`[START] ${task.name}`)
+            setTimeout(() => {
+                console.log(`[DONE] ${task.name} ${task.duration}ms`)
+                resolve(task.name)
+            }, task.duration);
+        })
+    }
+
+    const runQueue = async function () {
+        console.log('-- Tasks Queue --')
+        let taskQueue = []
+        for (const task of tasks) {
+            let result = await executeTask(task)
+            taskQueue.push(result)
+        }
+        console.log('-- All Tasks completed --')
+        console.log('Results:', taskQueue)
+
+    }
+    console.log(asyncTaskQueueTitle)
 }()
-
-
-
-
-
-
-
-

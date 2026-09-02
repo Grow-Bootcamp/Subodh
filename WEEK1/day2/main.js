189bde3a494de5c12abd1e60efeb5c52c4de5894
subodh <shahsubodh166@gmail.com>
2026-09-02 17:41:54 +0545

    complete coreMethodsDemo and timerDemo functions

--- WEEK1/day2/main.js
+++ WEEK1/day2/main.js
@@ -1,11 +1,11 @@
 //Topics
 
-let eventLoop = '== Event Loop =='
-let hoisting = '== Hoisting =='
-let coreMethods = '== Core Data types Methods =='
-let timers = '== Timers =='
-let promiseAsyncAwait = '== Asynchronous code in JS =='
-let asyncTaskEngineCli = '== Async Task Engine Cli =='
+const eventLoop = '== Event Loop =='
+const hoisting = '== Hoisting =='
+const coreMethods = '== Core Data types Methods =='
+const timers = '== Timers =='
+const promiseAsyncAwait = '== Asynchronous code in JS =='
+const asyncTaskEngineCli = '== Async Task Engine Cli =='
 
 
 const eventLoopDemo = function () {
@@ -87,24 +87,110 @@ const coreMethodsDemo = function () {
     // Array: map, filter, reduce, find, join, toString, slice, splice
     let demoArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    demoArray.map((num) => {
-        num * 10
-    })
-    console.log(demoArray)
 
-    demoArray.filter(num => {
-        num < 90
+    let arrayMap = demoArray.map((num) => {
+        return num * 10
     })
-    console.log(demoArray)
+    console.log(arrayMap)
+
+    let arrayFilter = arrayMap.filter(num => {
+        return num < 90
+    })
+    console.log(arrayFilter)
+
+    let arrayReduce = demoArray.reduce((a, c) => {
+        return a * c
+    }, 1)
+    console.log(arrayReduce)
+
+    let arrayFind = demoArray.find((number) => {
+        return number * number > 50
+    })
+    console.log(arrayFind)
+
+    let arrayJoin = demoArray.join("")
+    console.log(arrayJoin)
+
+    let arraySlice = demoArray.slice(2, 6)
+    console.log(arraySlice)
+
+    let arraySplice = demoArray.splice(5, 2)
+    console.log(demoArray + ": Original array after splice")
+    console.log(arraySplice + ": Spliced array")
+
 
     // Object: keys, values, entries
-    let demoObject = {};
+    let demoObject = {
+        fname: 'Subodh',
+        lname: 'Shah',
+        age: 21,
+        address: "Dhangadhi, Kailali",
+        contact: {
+            ncell: '9824692161',
+            ntc: ''
+        }
+    };
 
-    // String: includes, slice, trim, toLowercase. split, toLowerCase, toUpperCase
-    let demoString = 'Subodh is currently at CloudTech Dhangadhi branch'
+    console.log(Object.keys(demoObject))
+    console.log(Object.values(demoObject))
+    console.log(Object.entries(demoObject))
 
-    // Number: toString, toFixed, Math.floor, Math.round, Math.random 
+    // String: includes, slice, trim, toLowercase, split, toLowerCase, toUpperCase
+    let demoString = '   Subodh is currently at CloudTech Dhangadhi branch  '
+
+    console.log(demoString.includes('CloudTech'))
+    console.log(demoString.slice(0, 7))
+    console.log(demoString.trim())
+    console.log(demoString.toLowerCase())
+    console.log(demoString.toUpperCase())
+    console.log(demoString.split(' '))
+
+    // Number: toString, toFixed, Math.floor, Math.round, Math.random
+    let demoNumber = 99.99
+    console.log(demoNumber.toString())
+    console.log(demoNumber.toFixed(1))
+    console.log(Math.floor(demoNumber))
+    console.log(Math.round(demoNumber))
+    console.log(Math.random())
+
     console.log(coreMethods)
+}()
+
+
+const timersDemo = function () {
+    console.log(timers)
+    console.log('Timer start')
+
+    let delay = 3000;
+    setTimeout(() => {
+        console.log(`Timeout after ${delay}ms`)
+    }, delay)
+
+    let count = 0
+    let totalIntervalTime = 0
+    let intervalId = setInterval(() => {
+        count++
+        totalIntervalTime += delay
+        if (count > 1) {
+            clearInterval(intervalId)
+            console.log(`Interval stopped after ${totalIntervalTime}: Count = ${count}`)
+        }
+    }, delay)
+
+    console.log('Timer End - interval runs in background')
+}()
+
+const promiseAsyncAwaitDemo = function () {
+    console.log(promiseAsyncAwait)
+
+
+    console.log(promiseAsyncAwait)
+}()
+
+const asyncTaskEngineCliDemo = function () {
+    console.log(asyncTaskEngineCli)
+
+    console.log(asyncTaskEngineCli)
 }()
 
 

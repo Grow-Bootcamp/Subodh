cc85f2bf7c8d88de175f657ec3015b6862c7695f
subodh <shahsubodh166@gmail.com>
2026-09-08 16:22:26 +0545

    implement Restful API design principles and error handling

--- WEEK2/week2day1/main.js
+++ WEEK2/week2day1/main.js
@@ -93,7 +93,7 @@ function aFetchData() {
 
 async function getUserData() {
   try {
-    let data = await aFetchData(); // Perfoem some asynchronous operations
+    let data = await aFetchData(); // Perform some asynchronous operations
     if (data) console.log("Success: " + data);
   } catch (err) {
     console.error("Error: " + err.message);
@@ -153,4 +153,141 @@ class cStudent {
 let student2 = new cStudent("samit", 21, "B.Sc.CSIT", 7, subjectsFor7thSem);
 student2.getDetails();
 
-// RestFul URL Structure for a sample resource(users, products) following REST conventions
+// // RestFul URL Structure for a sample resource(users, posts) following REST convention
+// Using Promise
+
+// let resourceIds = [];
+// document.getElementById("GET").addEventListener("click", () => {
+//   //Get
+//   fetch("http://localhost:3000/posts")
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//     });
+// });
+
+// document.getElementById("POST").addEventListener("click", async () => {
+//   //POST
+//   return await fetch("http://localhost:3000/posts", {
+//     method: "POST",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       body: "post body",
+//       email: "post@email.com",
+//       name: "postBySubodh",
+//       postId: 0,
+//     }),
+//     headers: {
+//       "Content-type": "application/json; charset=UTF-8",
+//     },
+//   })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//       resourceIds = [...resourceIds, json.id];
+//     });
+// });
+
+// document.getElementById("PUT").addEventListener("click", () => {
+//   //PUT
+//   fetch(`http://localhost:3000/posts/${resourceIds[0]}`, {
+//     method: "PUT",
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({
+//       id: resourceIds[0],
+//       body: "put body",
+//       email: "put@email.com",
+//       name: "putBySubodh",
+//       postId: 0,
+//     }),
+//   })
+//     .then((res) => {
+//       return res.json();
+//     })
+//     .then((json) => {
+//       console.log(json);
+//     });
+// });
+
+// document.getElementById("DELETE").addEventListener("click", () => {
+//   //DELETE
+//   fetch(`http://localhost:3000/posts/${resourceIds[0]}`, {
+//     method: "DELETE",
+//   }).then((res) => {
+//     const success = res.ok;
+//     if (success) return res.json();
+//     else console.err("Bad Response");
+//   });
+// });
+
+const BASE_URL = "http://localhost:3000";
+
+async function runReq(method, path, data = null) {
+  document.getElementById("status").innerText = method + " Sending...";
+  document.getElementById("output").innerText = "";
+
+  const opts = { method, headers: { "Content-Type": "application/json" } };
+  if (data) opts.body = JSON.stringify(data);
+
+  try {
+    const res = await fetch(BASE_URL + path, opts);
+    if (res.ok) {
+      const json = await res.json();
+      document.getElementById("status").innerText =
+        `Status: ${res.status} ${res.statusText}`;
+      document.getElementById("output").innerText = JSON.stringify(
+        json,
+        null,
+        2,
+      );
+    } else throw new Error(`HTTP Error: ${res.status}:${res.statusText}`);
+  } catch (err) {
+    document.getElementById("status").innerText = "Error: " + err.message;
+  }
+}
+
+function runPost() {
+  runReq("POST", "/posts", {
+    title: `New Item ${Math.floor(Math.random() * 10)}`,
+  });
+}
+
+function runGet() {
+  runReq("GET", "/posts");
+}
+
+async function getFirstId() {
+  try {
+    const data = await fetch(BASE_URL + "/posts");
+    const json = await data.json();
+    const id = json[0].id;
+    if (id) return id;
+    else throw new Error("No resource found");
+  } catch (error) {
+    return error.message;
+  }
+}
+
+async function runPut() {
+  try {
+    let id = await getFirstId();
+    runReq("PUT", `/posts/${id}`, {
+      title: "Updated item",
+    });
+  } catch (error) {
+    console.log(`${error.message}`);
+  }
+}
+
+async function runDelete() {
+  try {
+    let id = await getFirstId();
+    runReq("DELETE", `/posts/${id}`);
+  } catch (error) {
+    console.log(`${error.message}`);
+  }
+}

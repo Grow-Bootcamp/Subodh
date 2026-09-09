ae6caa4dabb98e039c2c752111c5d89b3ea2d83b
subodh <shahsubodh166@gmail.com>
2026-09-09 16:12:37 +0545

    init OAuth folder with express server and google cloud OAuth api

--- WEEK2/week2day2/docker-demo/server.js
+++ WEEK2/week2day2/docker-demo/server.js
@@ -1,10 +1,29 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send(`Hello from Docker!!!`);
+  res.send(`
+    <h1>Test POST Request</h1>
+    <button onclick="sendPost()">Click to Send POST</button>
+    <script>
+      async function sendPost() {
+        const response = await fetch('/', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ user: "Docker User" })
+        });
+        const data = await response.json();
+        alert(JSON.stringify(data));
+      }
+    </script>
+  `);
 });
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000");
+app.post("/", (req, res) => {
+  console.log("POST Body Received:", req.body);
+  res.json({ status: "Success", data: req.body });
 });
+
+app.listen(4000, "0.0.0.0", () => console.log("Server running on port 4000"));

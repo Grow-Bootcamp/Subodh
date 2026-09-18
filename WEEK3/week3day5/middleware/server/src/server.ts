1f652747d92320df35c5438b7ad0731c7866c1f4
subodh <shahsubodh166@gmail.com>
2026-09-18 18:22:41 +0545

    feat: add authorization and logging api(just simulation)

--- WEEK3/week3day5/middleware/server/src/server.ts
+++ WEEK3/week3day5/middleware/server/src/server.ts
@@ -8,10 +8,12 @@ const logger = function (
   res: express.Response,
   next: express.NextFunction,
 ) {
-  console.log("This is logger Middleware");
+  console.log("This is logger Middleware getting request from: ");
   console.log(req.method, req.url);
   next(); // It is passing control to other middlewares or apis
 };
+// Logger middleware is being used here
+app.use(logger);
 
 // Authentication Middleware
 const auth = function (
@@ -19,16 +21,12 @@ const auth = function (
   res: express.Response,
   next: express.NextFunction,
 ) {
-  //   const token = req.headers.authorization; // Can be anything: Most commonly used JWT Token
-  //   if (!token) return res.status(401).send("Unauthorized");
+  const token = req.headers.authorization; // Can be anything: Q:Is most commonly used JWT Token?
+  if (!token) return res.status(401).send("Unauthorized");
   next();
 };
-
 // Authorization middleware used by passing it to the use method of express
 app.use(auth);
-
-// Logger middleware is being used here
-app.use(logger);
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send(`You requested from ${req.url} using ${req.method} method`);
@@ -39,4 +37,4 @@ app.get("/dashboard", (req: express.Request, res: express.Response) => {
   res.send(`[PLACEHOLDER] This is your DashBoard`);
 });
 
-app.listen(3000);
+app.listen(4000);
